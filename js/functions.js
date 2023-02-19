cb0821cbf9fa9fdf26f8861418a5dc2bda1d8650
GabzhRuslan <habjamilov7ok77.ru@mail.ru>
2023-02-19 14:48:38 +0500

    Решение задачи №2

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,28 @@
 'use strict';
 
+//Задание №1
+
 function checkStringLength (string, lengthLimit) {
   return string.length <= lengthLimit;
 }
 
 checkStringLength('Электродвигатель', 16);
+
+//Задание №2
+
+function checkPalindrome (string) {
+  let newString = '';
+
+  string = string.toLowerCase().replaceAll(' ', '');
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString === string;
+}
+
+checkPalindrome('топот'); //true
+checkPalindrome('ДовОд'); //true
+checkPalindrome('Кекс'); //false
+checkPalindrome('Лёша на полке клопа нашёл '); //true
