e370ccefc2bb1fb50f484df4ba158c996e39c10b
GabzhRuslan <habjamilov7ok77.ru@mail.ru>
2023-04-05 07:45:45 +0500

    Добавление новых данных id и description

--- js/rendering.js
+++ js/rendering.js
@@ -1,19 +1,20 @@
-import {createPhotoDescriptions} from './data.js';
-
-const dataFotos = createPhotoDescriptions();
 const pictureContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureFragment = document.createDocumentFragment();
 
-dataFotos.forEach(({url, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  const messages = comments.map((message) => message.message);
-  pictureElement.querySelector('.picture__comments').textContent = messages.length;
-  pictureFragment.append(pictureElement);
-});
+const assemblyElements = (photoDetails) => {
+  photoDetails.forEach(({url, likes, comments, id, description}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    const messages = comments.map((message) => message.message);
+    pictureElement.querySelector('.picture__comments').textContent = messages.length;
+    pictureElement.dataset.photoId = id;
+    pictureFragment.append(pictureElement);
+  });
 
-pictureContainer.append(pictureFragment);
+  pictureContainer.append(pictureFragment);
+};
 
-export {dataFotos};
+export {assemblyElements};
