b5b703588d5a05034df59f2ac4cb28ad93559176
GabzhRuslan <habjamilov7ok77.ru@mail.ru>
2023-04-07 04:56:53 +0500

    Изменение точки входа

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-import {createPhotoDescriptions} from './data.js';
-import {assemblyElements} from './rendering.js';
-import './rendering-full-image.js';
+import {addPictures} from './rendering.js';
 
-const photoDetails = createPhotoDescriptions();
-assemblyElements(photoDetails);
-
-export {photoDetails};
+addPictures();
