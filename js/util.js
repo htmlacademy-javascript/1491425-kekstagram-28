96fdda02e4b55b100b38d69de0011ca7a4ab21bb
GabzhRuslan <habjamilov7ok77.ru@mail.ru>
2023-04-07 04:58:30 +0500

    Создание функции isEscapeKey

--- js/util.js
+++ js/util.js
@@ -20,31 +20,8 @@ function createRandomIdFromRangeGenerator (min, max) {
   };
 }
 
-const isEscapeKey = (evt) => evt.key === 'Escape';
-const isEnterKey = (evt) => evt.key === 'Enter';
-
-const makeElement = (tagName, className, text) => {
-  const element = document.createElement(tagName);
-  element.classList.add(className);
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-};
-
-const createComment = (arr, container) => {
-  arr.forEach((item) => {
-    const listItem = makeElement('li', 'social__comment');
-    const picture = makeElement('img', 'social__picture');
-    picture.src = item.avatar;
-    picture.alt = item.name;
-    listItem.appendChild(picture);
-    const commentText = makeElement('p', 'social__text', item.message);
-    listItem.appendChild(commentText);
-    container.appendChild(listItem);
-  });
-};
-
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArrayElement, isEscapeKey, isEnterKey, createComment};
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArrayElement, isEscapeKey};
